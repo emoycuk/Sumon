dc839e0f9a512d629339a161ce1dc467007af347
EmirKilic <66028902+emoycuk@users.noreply.github.com>
2025-02-15 21:15:31 +0300

    My local changes before merge

--- Assets/Scripts/Player1Attack.cs
+++ Assets/Scripts/Player1Attack.cs
@@ -34,7 +34,7 @@ public class PlayerAttack : MonoBehaviour
     private void Attack()
     {
         // Optionally trigger an animation
-        // anim.SetTrigger("p1attack");
+        anim.SetTrigger("p1attack");
 
         // Reset cooldown
         cooldownTimer = 0f;
